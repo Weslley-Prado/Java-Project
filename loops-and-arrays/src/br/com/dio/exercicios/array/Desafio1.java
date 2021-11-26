9a0b9617dd9f059dc95ce6fa50d26ec7689988fe
Weslley-Prado <84636703+Weslley-Prado@users.noreply.github.com>
2021-11-26 14:01:08 -0300

    Create Desafio1.java

---
+++ loops-and-arrays/src/br/com/dio/exercicios/array/Desafio1.java
@@ -0,0 +1,20 @@
+package br.com.dio.exercicios.array;
+
+import java.util.Scanner;
+
+public class Desafio1 {
+
+	
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			
+			int L = sc.nextInt();
+			int C = sc.nextInt();
+			
+			if (( L==6)&&(C==9))
+				System.out.println("  0 ");
+			else                                               //complete o código nos espaços em branco
+				System.out.println(" 1 ");
+			sc.close();
+		}
+	}
