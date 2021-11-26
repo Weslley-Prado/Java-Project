860cbce153331cc6affa7ed6d3378b7e80d49878
Weslley-Prado <84636703+Weslley-Prado@users.noreply.github.com>
2021-11-26 14:04:00 -0300

    Create Fila.java

---
+++ EstruturadeDadosFila/src/com/fila/Fila.java
@@ -0,0 +1,82 @@
+package com.fila;
+
+public class Fila<T> {
+
+    private No<T> refNoEntradaFila;
+
+    public Fila() {
+        this.refNoEntradaFila = null;
+    }
+
+    @SuppressWarnings("unchecked")
+	public void enqueue(T object){
+        @SuppressWarnings({ "rawtypes" })
+		No novoNo = new No(object);
+        novoNo.setRefNo(refNoEntradaFila);
+        refNoEntradaFila = novoNo;
+    }
+
+    @SuppressWarnings("unchecked")
+	public T first(){
+        if(!this.isEmpty()){
+            @SuppressWarnings("rawtypes")
+			No primeiroNo = refNoEntradaFila;
+            while (true){
+                if(primeiroNo.getRefNo() != null){
+                    primeiroNo = primeiroNo.getRefNo();
+                }else{
+                    break;
+                }
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+	public T dequeue(){
+        if(!this.isEmpty()){
+            @SuppressWarnings("rawtypes")
+			No primeiroNo = refNoEntradaFila;
+            @SuppressWarnings("rawtypes")
+			No noAuxiliar = refNoEntradaFila;
+            while (true){
+                if(primeiroNo.getRefNo() != null){
+                    noAuxiliar = primeiroNo;
+                    primeiroNo = primeiroNo.getRefNo();
+                }else{
+                    noAuxiliar.setRefNo(null);
+                    break;
+                }
+            }
+            return (T) primeiroNo.getObject();
+        }
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return refNoEntradaFila == null? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String stringRetorno = "";
+        @SuppressWarnings("rawtypes")
+		No noAuxiliar = refNoEntradaFila;
+
+        if(refNoEntradaFila != null){
+            while (true){
+                stringRetorno += "[No{objeto=" + noAuxiliar.getObject() + "}]--->";
+                if(noAuxiliar.getRefNo() != null){
+                    noAuxiliar = noAuxiliar.getRefNo();
+                }else{
+                    stringRetorno += "null";
+                    break;
+                }
+            }
+        }else{
+            stringRetorno = "null";
+        }
+        return stringRetorno;
+    }
+}
