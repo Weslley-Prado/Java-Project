2db6947c17546a978cd58bc9810a86d0fed6805c
Weslley-Prado <84636703+Weslley-Prado@users.noreply.github.com>
2021-11-26 14:00:31 -0300

    Create Ex1Nome.java

---
+++ loops-and-arrays/src/br/com/dio/exercicios/loops/Ex1Nome.java
@@ -0,0 +1,24 @@
+package br.com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Ex1Nome {
+
+	public static void main(String[]args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String nome;
+		@SuppressWarnings("unused")
+		int idade;
+		while(true) {
+			System.out.println("Nome:");
+			nome =scan.next();
+			if (nome.equals("0")) break;
+			System.out.println("Idade:");
+			idade = scan.nextInt();			
+		}
+		
+		System.out.println("Continua...");
+	}
+	
+}
